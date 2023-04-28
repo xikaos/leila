157ae7e780fab80fc4d51c0ac8f8c16552a2535e
Francisco Budaszewski Zanatta <xikaoswow@gmail.com>
2023-04-27 22:03:46 -0300

    Refactor auction constructor and get auctions return value (#5)
    * Extract Auction class from main script
    
    * Refactor Auction constructor. Refactor get_auctions
    
    * Add missing EOF on both files
    
    ---------
    
    Co-authored-by: Francisco Budaszewski Zanatta <francisco.zanatta@ciandt.com>

---
+++ auction.rb
@@ -0,0 +1,44 @@
+class Auction
+    attr_accessor :name, :initial_price, :bids, :buyout_price, :seller, :reference_count, :positive_references, :expires_in
+
+    def initialize auction_data
+      @name = auction_data[:name]
+      @initial_price = auction_data[:initial_price]
+      @bids = auction_data[:bids]
+      @buyout_price = auction_data[:buyout_price]
+      @seller = auction_data[:seller]
+      @reference_count = auction_data[:reference_count]
+      @positive_references = auction_data[:positive_references]
+      @expires_in = auction_data[:expires_in]
+    end
+
+    def print_offer
+      separator = '------------------------------'
+
+      puts separator
+      puts "Name ~> #{@name}"
+
+      puts separator
+      puts "Initial Price ~> #{@initial_price}"
+
+      puts separator
+      puts "Buyout Price ~> #{@buyout_price}"
+
+      puts separator
+      puts "Bids ~> #{@bids}"
+
+      puts separator
+      puts "Seller ~> #{@seller}"
+
+      puts separator
+      puts "References ~> #{@reference_count}"
+
+      puts separator
+      puts "Positive References ~> #{@positive_references}"
+
+      puts separator
+      puts "Expires In ~> #{@expires_in}"
+
+      puts separator + "\n\n"
+    end
+end
