fe15e5782c2fb81ad008ed4309cb556998e7fc86
Francisco Budaszewski Zanatta <francisco.zanatta@ciandt.com>
2023-04-29 18:33:56 -0300

    Small fix on Auction initialization test description

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,7 +3,7 @@ require_relative '../auction'
 
 describe Auction do
     describe '#new' do
-        it "Its created with the values I expect" do
+        it "Is created with the values I expect" do
             @values = {
                 name: 'Magic Card',
                 initial_price: 'R$ 10,00',
