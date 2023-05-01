69f2099833c78c86d314997b39a8fb794a4f07a1
Francisco Budaszewski Zanatta <xikaoswow@gmail.com>
2023-05-01 19:09:23 -0300

    Add first unit test (#7)
    * Add rspec gem and init run folders
    
    * Add to_h method on Auction for convenience
    
    * Add test for Auction initialization
    
    ---------
    
    Co-authored-by: Francisco Budaszewski Zanatta <francisco.zanatta@ciandt.com>

---
+++ spec/auction_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require_relative '../auction'
+
+require 'pry';
+
+describe Auction do
+    describe '#new' do
+        it "Its created with the values I expect" do
+            @values = {
+                name: 'Magic Card',
+                initial_price: 'R$ 10,00',
+                bids: 10,
+                buyout_price: 'R$ 20,00',
+                seller: 'Card Seller',
+                reference_count: '42',
+                positive_references: '100%',
+                expires_in: '17h 32m'
+            }
+
+            auction = Auction.new(@values)
+
+            expect(auction.to_h).to eq(@values)
+        end
+    end
+end
